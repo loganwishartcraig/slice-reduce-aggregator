0486da642d5c3fbdd39c5c22b50429db69c84171
Logan Wishart-Craig <lwishartcraig@aaaflag.com>
2019-11-12 17:05:42 -0800

    Trying different jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,25 @@
-// jest.config.js
-// const { defaults: tsjPreset } = require('ts-jest/presets');
-const { jsWithTs: tsjPreset } = require('ts-jest/presets');
-// const { jsWithBabel: tsjPreset } = require('ts-jest/presets');
+// // jest.config.js
+// // const { defaults: tsjPreset } = require('ts-jest/presets');
+// const { jsWithTs: tsjPreset } = require('ts-jest/presets');
+// // const { jsWithBabel: tsjPreset } = require('ts-jest/presets');
+
+// module.exports = {
+//   // [...]
+//   transform: {
+//     ...tsjPreset.transform,
+//     // [...]
+//   }
+// }
 
 module.exports = {
-  // [...]
-  transform: {
-    ...tsjPreset.transform,
-    // [...]
-  }
+  "roots": [
+    "<rootDir>/lib"
+  ],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)?$": "ts-jest"
+  },
 }
